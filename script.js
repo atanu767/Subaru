b27432a70601d03a7429a8368ce1e6837597013c
saheb1989 <saheb.jana14@gmail.com>
2025-09-22 15:26:17 +0530

    initial commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: false,
         }
@@ -21,7 +21,7 @@ $(document).ready(function () {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
